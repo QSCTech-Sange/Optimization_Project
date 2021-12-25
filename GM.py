4f10a029ca26ec292ae62dd3983660ed6869b3ca
Youcong Lu <1516116319@qq.com>
2021-12-25 16:37:11 +0800

    [ADD] GM_BB Step Strategy

--- GM.py
+++ GM.py
@@ -10,6 +10,13 @@ def backtrack(X,func,gX,d,alpha=1,gamma=0.01,sigma=0.5):
         alpha = alpha * sigma
     return alpha
 
+def BB(X,X_1,gX,gX_1):
+    sk_1 = mat2vec(X) - mat2vec(X_1)
+    y_1 = mat2vec(gX) - mat2vec(gX_1)
+    alpha = sk_1.T.dot(sk_1) / (sk_1.T.dot(y_1))
+    return alpha
+
+
 def GM(X,func,grad,tol):
     gX = grad(X)
     norm_2 = norm2(gX)
@@ -22,3 +29,23 @@ def GM(X,func,grad,tol):
         norm_2 = norm2(gX)
         loss.append(norm_2)
     return X,loss
+
+def GM_BB(X,func,grad,tol):
+    gX = grad(X)
+    iter = 0
+    norm_2 = norm2(gX)
+    loss = [norm_2]
+    tol = tol ** 2
+    while norm_2 > tol:
+        if iter ==0:
+            step_size = backtrack(X,func,gX,-gX)
+        else:
+            step_size = BB(X,X_1,gX,gX_1)
+        X_1 = X    
+        X = X - step_size * gX
+        gX_1 = grad(X_1)
+        gX = grad(X)
+        norm_2 = norm2(gX)
+        loss.append(norm_2)
+        iter = iter +1
+    return X,loss,iter
