b9518bcb62130784b87082331b336f4a84d4aeab
杨宇昊 <3160105521@zju.edu.cn>
2021-12-28 21:22:32 +0800

    add AGM weighted

--- AGM.py
+++ AGM.py
@@ -26,3 +26,25 @@ def AGM(grad,x0,step_size,tol):
         loss = norm(ngrad)
         losses.append(loss)
     return x,losses
+
+def AGM_weighted(grad,x0,step_size,tol,k,constant):
+    t_0 = t = 1
+    x = x_0 = x0
+    B = gen_B(x.shape[0],sparse=False)
+    W = gen_W(x,k,constant)
+    loss = norm(grad(x,B,W))
+    losses = [loss]
+    while loss > tol:
+        # get beta_k (beta, t均是标量)
+        beta = (t_0 - 1) / t
+        t_0 = t
+        t = 0.5 * (1 + np.sqrt(1 + 4 * t_0**2))
+        # update x
+        y = x + beta * (x - x_0)
+        x_0 = x
+        ngrad= grad(y,B,W)
+        x = y - step_size * ngrad
+        loss = norm(ngrad)
+        losses.append(loss)
+        # print(loss)
+    return x,losses
